09c6703ab29d4b8ce8ac1e4d97bf5a004648083b
Ousmanou-Ardo <63662669+Ousmanou-Ardo@users.noreply.github.com>
2021-10-11 06:47:06 -0400

    updated the api url
    updated the api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3182348b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
